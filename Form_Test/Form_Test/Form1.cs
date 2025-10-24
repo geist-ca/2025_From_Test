e822faf70b78193356d97a1d8a1076e1e76ae800
GEIST\zinbe <zinbei.4286@gmail.com>
2025-10-24 15:34:52 +0900

    フォーム作成

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Form_Test
 {
@@ -15,6 +16,23 @@ namespace Form_Test
         public Form1()
         {
             InitializeComponent();
+            Test_Button test_Button = new Test_Button();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+
+                    test_Button.Location = new Point(50 * i, 50 * j);
+                    test_Button.Size = new Size(50, 50);
+                    test_Button.Text = "あいうえお";
+                    Controls.Add(test_Button);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("joe");
         }
     }
 }
