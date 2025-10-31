8e3cb1814da520e38ba905ec1b40df967137ed6c
GEIST\zinbe <zinbei.4286@gmail.com>
2025-10-31 15:34:09 +0900

    色変更

--- Form_Test/Form_Test/Test_Button.cs
+++ Form_Test/Form_Test/Test_Button.cs
@@ -10,13 +10,17 @@ using System.Windows.Forms;
 
 namespace Form_Test
 {
-    internal class Test_Button :Button
+    internal class Test_Button : Button
     {
-        private Color _Oncolor=Color.White;
-        private Color _Offcolor=Color.Red;
+        private Color _Oncolor = Color.White;
+        private Color _Offcolor = Color.Black;
+
+        private bool _enable;
+
 
         public void SetEnable(bool on)
-            {
+        {
+            _enable = on;
             if (on)
             {
                 BackColor = _Oncolor;
@@ -26,25 +30,25 @@ namespace Form_Test
             {
                 BackColor = _Offcolor;
             }
-            }
-        public Test_Button(Point position,Size size,string text) 
-        {
-        Location= position;
-        Size = size;
-        Text = text;
-
-
-            Click += button1_Click;
-
         }
-        private void button1_Click(object sender, EventArgs e)
+        public Test_Button(Point position, Size size, string text)
         {
-            MessageBox.Show("joe");
+            Location = position;
+            Size = size;
+            Text = text;
+
+
+            Click += ClickEvent;
+            SetEnable(false);
         }
 
 
+        private void ClickEvent(object sender, EventArgs e)
+        {
+        SetEnable(!_enable);
+            
 
-
-
+            
+        }
     }
 }
